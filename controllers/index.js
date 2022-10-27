21be97a0ae94df070a22f50aa3b30bb5a0f413c2
tmorgan1987 <tmorgan1987@outlook.com>
2022-10-27 16:25:21 -0400

    boys insomnia this right here

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const dogRoutes = require('./dog-routes');
-const userRoutes = require('./user-routes');
+// const dogRoutes = require('./dog-routes');
+// const userRoutes = require('./user-routes');
 
 router.use('/api', apiRoutes);
-router.use('/', userRoutes);
-router.use('/dog', dogRoutes);
+// router.use('/', userRoutes);
+// router.use('/dog', dogRoutes);
 
 module.exports = router;
