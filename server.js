6192ca9b71ed0e1794d791fc95ded6f9c0ef2614
devWeibel <weibel.jason@gmail.com>
2022-10-27 11:33:55 -0400

    rendering handlebars

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const path = require('path');
 const sequelize = require('./config/connection');
+const handlebars = require('express-handlebars');
 // const Model = require("./models");
 
 const app = express();
+app.set('view engine', 'handlebars');
 
 const PORT = process.env.PORT || 3001;
 
 // Sets static folder to public.
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/', (req, res) => {
-    res.send('<h1>Hello!  Welcome to the Muttel</h1>'); // test message, delete later
+    res.render('main', {layout : 'index'});
 });
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => {
-        console.log(`Listening on Port ${PORT}`);
+        console.log(`Listening on Port ${PORT} at http://localhost:3001`);
     });
 });
-
