ca75da8e7a1922639dd2a5b7483eea7aa7130d97
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-27 14:26:10 -0400

    fixed user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,28 @@
 const router = require('express').Router();
+const { User, Dog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            include: [{model: Dog}],
+        });
+        console.log(userData);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }  
+});
+
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body);
+        const userData = await User.create(req.body);
+        res.status(200).json({ msg: "Your account has been created!", userData});
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 
 module.exports = router;
