cc27fc7c48fde744e0e81fe5aae83b3af41132d9
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-28 14:07:46 -0400

    login and signup handlebars and js

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,55 @@
 const loginForm = async function(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username');
-    const password = document.querySelector('#passowrd');
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#passowrd-login').value.trim;
 
+    if (username && password) {
+        //Send a POST request to the API endpoint
     const response = await fetch('/api/user/login', {
         method: 'POST', 
         body: JSON.stringify({
             username: username.value,
             password: password.value,
         }),
-        headers: { 'Content-Type': ''}
+        headers: { 'Content-Type': 'application/json'},
+    });
+    
+    if (response.ok) {
+        document.location.replace('/main');
+    } else {
+        alert('Failed to login');
+        };
+    };
+};
 
+const signUpForm = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
         }
-    })
+    }
+};
 
-}
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginForm);
+
+document
+    .querySelector('.singup-form')
+    .addEventListener('submit', signUpForm);
+
