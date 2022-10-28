6d1c4c7b2865bd5355a61f5ca49974f1e68960d5
devWeibel <weibel.jason@gmail.com>
2022-10-28 12:04:29 -0400

    fixed res.render

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.render('main', {layout : 'index'});
+    res.render('home', {layout : 'main'});
 });
 
 //app.use(controllers);
