f494188dc2f423ec5577d68984250e8284abc2ee
tmorgan1987 <tmorgan1987@outlook.com>
2022-10-29 21:48:31 -0400

    html previews for visual reference, yo handlebars now open with liveserver

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,7 @@ const apiRoutes = require('./api');
 const dogRoutes = require('./dog-routes');
 const userRoutes = require('./user-routes');
 
-router.use('/api', apiRoutes);
-router.use('/', userRoutes);
+router.use('/user', userRoutes);
 router.use('/dog', dogRoutes);
 
 module.exports = router;
