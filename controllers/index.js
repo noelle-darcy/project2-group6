5ff0acb0b207a673446225430e21171fbbbd8326
devWeibel <weibel.jason@gmail.com>
2022-10-30 22:00:11 -0400

    added possibly useful passport and bycrypt stuff

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-
 const homeRoutes = require('./home-routes');
+
+// Renders Landing.handlebars when user navigates to root '/'. 
+router.get('/', (req,res) => {
+    res.render('landing');
+});
 
 router.use('/', homeRoutes);
 
